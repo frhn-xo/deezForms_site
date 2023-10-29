18a14260bddf80956eb28ed9593cd8944e7a51d3
raaasin <nisarvskp@gmail.com>
2023-10-29 07:33:18 +0530

    updates

--- script.js
+++ script.js
@@ -79,3 +79,40 @@ document.addEventListener("DOMContentLoaded", function() {
                 console.error("Error:", error);
             });
     });});
+    document.addEventListener("DOMContentLoaded", function() {
+        document.getElementById("data-entry").addEventListener("submit", function (e) {
+            e.preventDefault(); // Prevent the default form submit action
+            console.log("hi"); // Log "hi" to the console
+    
+            const link = document.getElementById("link").value;
+            const git = document.getElementById("git").value;
+            const emailid = localStorage.getItem("emailid");
+            // Create a JSON object with email and password
+            const userData = {
+                emailid: emailid,
+                linkedin:link,github:git
+            };
+            console.log(userData)
+            fetch("http://192.168.9.140:5000/api/newuser"
+            , {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify(userData),
+            })
+                .then(response => response.json())
+                .then(data => {
+                    if (data === true) {
+                        // If the response is true, add "emailid" to local storage
+                        // Redirect to the after login page
+                        window.location.href = "fourth.html";
+                    } else {
+                        // If the response is false, display an error message
+                        console.log("error");
+                    }
+                })
+                .catch(error => {
+                    console.error("Error:", error);
+                });
+        });});
